a8cd6997cedbfa1999e7c5d7e90a38bbb72c992f
brandonkimmmm <bkim2490@gmail.com>
2023-03-09 14:17:28 +0900

    Day 20-21: Snake Game

---
+++ day-20-21-snake-game/main.py
@@ -0,0 +1,51 @@
+from time import sleep
+from turtle import Screen
+
+from food import Food
+from scoreboard import Scoreboard
+from snake import Snake
+
+game_is_over = False
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
+
+screen.listen()
+
+
+while not game_is_over:
+    screen.update()
+    sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increment_score()
+
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
+        game_is_over = True
+        scoreboard.game_over()
+
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_over = True
+            scoreboard.game_over()
+
+screen.exitonclick()
---
+++ day-20-21-snake-game/snake.py
@@ -0,0 +1,50 @@
+from turtle import Screen, Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITIONS:
+            self.add_segment(pos)
+
+    def move(self):
+        for seg_index in range(len(self.segments) - 1, 0, -1):
+            self.segments[seg_index].goto(self.segments[seg_index - 1].pos())
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def add_segment(self, position):
+        seg = Turtle(shape="square")
+        seg.color("white")
+        seg.penup()
+        seg.setposition(position)
+        self.segments.append(seg)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
