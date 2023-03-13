858e386d0a0cc36aea5625359b1b57f326bad8af
brandonkimmmm <bkim2490@gmail.com>
2023-03-13 14:52:42 +0900

    snake add high score

--- day-20-21-snake-game/scoreboard.py
+++ day-20-21-snake-game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.penup()
         self.color("white")
         self.hideturtle()
@@ -16,12 +17,18 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(
+            arg=f"Score: {self.score} High Score: {self.high_score}",
+            align=ALIGNMENT,
+            font=FONT,
+        )
 
     def increment_score(self):
         self.score += 1
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
--- day-20-21-snake-game/snake.py
+++ day-20-21-snake-game/snake.py
@@ -39,6 +39,13 @@ class Snake:
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def add_segment(self, position):
         seg = Turtle(shape="square")
         seg.color("white")
