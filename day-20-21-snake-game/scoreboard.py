1ff163d9e4f714b8742d26e504c7f210853f1d9b
brandonkimmmm <bkim2490@gmail.com>
2023-03-13 15:17:45 +0900

    Snake game persist high score

--- day-20-21-snake-game/scoreboard.py
+++ day-20-21-snake-game/scoreboard.py
@@ -1,5 +1,7 @@
+from os import path
 from turtle import Turtle
 
+FILE_PATH = f"{path.dirname(__file__)}/data.txt"
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
@@ -7,13 +9,21 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = 0
-        self.high_score = 0
-        self.penup()
-        self.color("white")
-        self.hideturtle()
-        self.goto(0, 270)
-        self.update_score()
+        if not path.isfile(FILE_PATH):
+            file = open(FILE_PATH, mode="w")
+            file.write("0")
+            file.close()
+        with open(FILE_PATH) as file:
+            score = file.read()
+            if score == "":
+                score = "0"
+            self.score = 0
+            self.high_score = int(score)
+            self.penup()
+            self.color("white")
+            self.hideturtle()
+            self.goto(0, 270)
+            self.update_score()
 
     def update_score(self):
         self.clear()
@@ -30,5 +40,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open(FILE_PATH, mode="w") as file:
+                file.write(str(self.high_score))
         self.score = 0
         self.update_score()
